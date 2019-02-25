41739c0f44b7ffb33cde379eee1527ee04869080
christopher.fernandes <christyabcd.05@gmail.com>
2019-02-25 18:20:44 +0530

    added button component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -69,6 +69,14 @@ export class HomePage {
       "pageName": DetailCardComponent
     },
     {
+      "name":"Popups",
+      "pageName": PopupsComponent
+    },
+    {
+      "name":"Buttons",
+      "pageName": ButtonsComponent
+    },
+    {
       "name":"BreadCrumb Card",
       "pageName": BreadcrumbsComponent
     },
